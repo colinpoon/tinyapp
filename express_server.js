6ff6d3e744cdeb921cbfd891a4a4cbfe9cd5c651
Colin Poon <me@colinpoon.com>
2021-09-27 18:23:13 +0000

    morgan

--- express_server.js
+++ express_server.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
 
+//  MIDDLEWARE
+app.use(morgan('dev'));
+
+//  DATABASES
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//  GET HOME
 app.get("/home", (req, res) => { // (request, response)
   res.send("Hello!");
 });
-
+//  GET URLDATABASE IN JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-///////////
+// DEFAULT // CATCH - ALL
+app.get ('*', (req, res) => {
+  res.status(404).send('Error'); // unhappy path
+})
+
+//_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(
+//   _.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(
 app.listen(PORT, () => {
   console.log(`On PORT ${PORT}...`);
 });
