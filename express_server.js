0549f919365435c8ebed92775bc0077a88494868
Colin Poon <me@colinpoon.com>
2021-09-27 18:28:21 +0000

    commit to proceed

--- express_server.js
+++ express_server.js
@@ -16,6 +16,11 @@ const urlDatabase = {
 app.get("/home", (req, res) => { // (request, response)
   res.send("Hello!");
 });
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 //  GET URLDATABASE IN JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -24,7 +29,7 @@ app.get("/urls.json", (req, res) => {
 // DEFAULT // CATCH - ALL
 app.get ('*', (req, res) => {
   res.status(404).send('Error'); // unhappy path
-})
+});
 
 //_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(
 //   _.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(_.~"(
